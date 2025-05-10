4e68ec3d4ffb5b976ad0f3b4108228c53d39dd65
Lanfix <raphael.chapotard@outlook.fr>
2025-05-10 16:21:00 +0200

    Fixed a tracker duplication bug

--- src/main/java/fr/lanfix/randomitemchallenge/game/Tracker.java
+++ src/main/java/fr/lanfix/randomitemchallenge/game/Tracker.java
@@ -3,8 +3,8 @@ package fr.lanfix.randomitemchallenge.game;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.CompassMeta;
 
 public class Tracker {
@@ -34,12 +34,13 @@ public class Tracker {
 
     public static void trackLocation(Player player, Location location, String name) {
         // this method either updates the players compass or gives a new compass to the player
-        Inventory inv = player.getInventory();
+        PlayerInventory inv = player.getInventory();
         if (inv.contains(Material.COMPASS)) {
             ItemStack item = inv.getItem(inv.first(Material.COMPASS));
             assert item != null;
-            // TODO Only update compass with the right name
             updateCompass(item, location);
+        } else if (inv.getItemInOffHand().getType().equals(Material.COMPASS)) {
+            updateCompass(inv.getItemInOffHand(), location);
         } else {
             ItemStack item = getCompass(location, name);
             if (inv.getItem(8) == null) {
